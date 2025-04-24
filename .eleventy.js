6700aa17edffd7190d0a31a1cbf1898874853601
hunterAlbertJ <albert.hunter13@gmail.com>
2025-04-24 00:47:52 -0700

    adding styling back in to pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,9 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/styles.css");
-
-  // Also copy JSON and CSV product data
-  eleventyConfig.addPassthroughCopy({ "data/products.json": "data/products.json" });
-  eleventyConfig.addPassthroughCopy({ "data/products": "data/products" });
+  eleventyConfig.addPassthroughCopy("src/styles.css");           
+  eleventyConfig.addPassthroughCopy({ "data/products.json": "data/products.json" }); 
 
   return {
+    pathPrefix: "/fedramp-marketplace-lite", 
     dir: {
       input: "src",
       includes: "_includes",
