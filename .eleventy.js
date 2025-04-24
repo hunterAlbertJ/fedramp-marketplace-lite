8a35d8b7035af1390d92a0cd4fe212651990ad42
hunterAlbertJ <albert.hunter13@gmail.com>
2025-04-24 00:17:11 -0700

    Fix: Add passthrough copy for styles and product data

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/styles.css");           
-  eleventyConfig.addPassthroughCopy({ "data/products.json": "data/products.json" }); 
+  eleventyConfig.addPassthroughCopy("src/styles.css");
+
+  // Also copy JSON and CSV product data
+  eleventyConfig.addPassthroughCopy({ "data/products.json": "data/products.json" });
+  eleventyConfig.addPassthroughCopy({ "data/products": "data/products" });
 
   return {
     dir: {
